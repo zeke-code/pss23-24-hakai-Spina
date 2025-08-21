767e90f81c27d6019f1e35947c72a46e8f3fc7db
zeke-code <ezechiele.spina@gmail.com>
2025-08-21 21:55:42 +0200

    chore: setup JavaFX, package structure, other dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,19 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
+
     // Add Spotless for linting and code style
     id("com.diffplug.spotless") version "7.2.1"
+
+    // JavaFX plugin to install JavaFX library needed modules
+    id("org.openjfx.javafxplugin") version "0.1.0"
+
+    /*
+    * Plugin to create fat Jar
+    * To create the fat jar, run on a terminal "./gradlew shadowJar
+    * The fat jar will be found in build/libs/projectname-all.jar
+    */
+    id("com.gradleup.shadow") version "9.0.2"
 }
 
 repositories {
@@ -16,7 +27,7 @@ dependencies {
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
-    // This dependency is used by the application.
+    // Guava EventBus might be needed in the future.
     implementation(libs.guava)
 }
 
@@ -29,7 +40,7 @@ java {
 
 application {
     // Define the main class for the application.
-    mainClass = "org.example.App"
+    mainClass = "com.zekecode.hakai.App"
 }
 
 spotless {
@@ -39,6 +50,17 @@ spotless {
         removeWildcardImports()
         googleJavaFormat()
     }
+}
+
+javafx {
+    version = "24.0.2"
+    modules = listOf(
+        "javafx.controls",
+        "javafx.base",
+        "javafx.fxml",
+        "javafx.swing",
+        "javafx.graphics"
+    )
 }
 
 tasks.named<Test>("test") {
